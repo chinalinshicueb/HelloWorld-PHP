84406d00024f9b16d3f69e44660cb46ab0729ea8
Peter Ren <public_me@163.com>
2016-10-19 10:52:01 +0800

    修改提示为中文

--- readbigdata.php
+++ readbigdata.php
@@ -3,12 +3,12 @@
 	$filename = "data/bigdata.txt";
 	$datastring = file_get_contents($filename);
 	if($datastring != null){
-		echo "string size is ".strlen($datastring);
+		echo "读取文件内容到字符串中，字符串大小： ".strlen($datastring);
 	}else{
 		echo "read big data failed.";
 	}
 	
-	echo "<hr>file method (read data to an array from a file)";
+	echo "<hr>file method (从文件中读取数据到数组中)";
 	$lines=file($filename);
 	echo "<p>lines size is ".sizeof($lines);
 ?>
