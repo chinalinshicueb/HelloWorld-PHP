e49b8e1c522cd0bf05098be05d30533818e4c75a
Peter Ren <public_me@163.com>
2016-10-20 14:50:39 +0800

    测试md5(file),sha1(file)和好压工具结果一致

--- testsecurity/md5sha1.php
+++ testsecurity/md5sha1.php
@@ -1,9 +1,19 @@
 <?php
 	$rawData = "我的数据abc123";
+	$rawDataFilename = "../index.php";
 	
 	echo "<p>md5($rawData):".md5($rawData);
 	echo "<p>md5($rawData, true):".md5($rawData, true);
 	
+	
 	echo "<p>sha1($rawData):".sha1($rawData);
 	echo "<p>sha1($rawData, true):".sha1($rawData, true);
+	
+	
+	echo "<p>md5_file($rawDataFilename):".md5_file($rawDataFilename);
+	echo "<p>md5_file($rawDataFilename, true):".md5_file($rawDataFilename, true);
+	
+	
+	echo "<p>sha1_file($rawDataFilename):".sha1_file($rawDataFilename);
+	echo "<p>sha1_file($rawDataFilename, true):".sha1_file($rawDataFilename, true);
 	
